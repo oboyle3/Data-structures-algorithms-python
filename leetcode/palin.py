d0569b044425beedc0c6570199202c95618266bd
oboyle3 <po720999@sju.edu>
2025-03-19 21:25:35 -0400

    update

--- leetcode/palin.py
+++ leetcode/palin.py
@@ -6,7 +6,7 @@ def palin(s):
     for i in range(length // 2):#only need to check till middle
         left_side = s[i]
         right_side = s[length -1 -i]
-        print(f"left side = {left_side} || right side = {right_side}")
+        print(f" {left_side} || {right_side}")
 
 
 somestring = "hello world"
