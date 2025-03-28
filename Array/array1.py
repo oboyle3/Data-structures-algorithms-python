37e94d029468dbe9a061b1ba8cc06097934713a3
oboyle3 <po720999@sju.edu>
2025-03-27 21:41:57 -0400

    a array

--- Array/array1.py
+++ Array/array1.py
@@ -2,10 +2,14 @@ my_list = [1,2,3,4,5]
 my_str_list = ["apple", "banana", "cherry"]
 
 print(my_list)
-print(my_list[0])
+#print(my_list[0])
 my_list.append(6)
-print(my_list)
+#print(my_list)
 def function(arr):
-    n = my_list
+    n = len(my_list)
     for i in range(n):
-        print(arr[i])
+        if arr[i] == 3:
+            print(f"hello world")
+    
+
+print(f"the result {function(my_list)}")
